19629497418bab45be7e0630ee490ebb578f6c65
Omar <omar.tarek29@msa.edu.eg>
2022-07-22 16:05:35 +0200

    Added skelton code for VideoExtractor

--- Extractor.py
+++ Extractor.py
@@ -26,5 +26,5 @@ class CExtractor:
 
     def GetLandMarks(self,image):
         pass
-    
+
 
---
+++ VideoExtraction.py
@@ -0,0 +1,7 @@
+from Extractor import *
+class VideoExtractor:
+    def __init__(self):
+        self.Extractor = CExtractor()
+    def GetLandMarksInVideo(self,videoPath):
+        pass
+    
